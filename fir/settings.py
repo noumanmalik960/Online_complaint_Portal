8202e5f56f66c8a0f2f8823febb9334ac2c80520
noma <noumanmalik960@gmail.com>
2019-12-23 00:20:18 +0500

    updated

--- fir/settings.py
+++ fir/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'noumanmalik960@gmail.com'
-EMAIL_HOST_PASSWORD = 'nuttertool'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 
 
 # Settings for django-bootstrap4
